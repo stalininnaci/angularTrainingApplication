d76d0674540e6d3b5d37f2c4d53a4f118b7dacaf
stalininnaci <stalininnaci0804@gmail.com>
2022-09-19 12:39:52 +0530

    applied colors

--- src/app/main-area/main-area.component.ts
+++ src/app/main-area/main-area.component.ts
@@ -6,24 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-area.component.scss']
 })
 export class MainAreaComponent implements OnInit {
-  reprint:any;
-  isdisabled=true;
-  userinput:any;
+  
+  // userinput:any;
 maintitle:any="main area working-"
+reprint:any;
+  isdisabled=true;
   constructor() { }
 
   ngOnInit(): void {
   }
 onbtnclick(){
   this.isdisabled=true
-   this.userinput="";
+  //  this.userinput="";
    this.reprint="";
   console.log("button working");
 }
 onUserInput(event:any){
   // this.reprint=event.target.value
   this.isdisabled=false
- this.userinput=event.target.value
+//  this.userinput=event.target.value
 // console.log();
 }
 toggle(){
